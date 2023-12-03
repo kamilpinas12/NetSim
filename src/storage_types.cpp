85b11e65e36f54d5e4019455ce4e303b4ffe55e6
MRach3451 <127220659+MRach3451@users.noreply.github.com>
2023-12-03 20:09:50 +0100

    Napisanie nowej metody w storage_types.cpp

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,3 +1,28 @@
 //
 // Created by kamil on 30/11/2023.
 //
+
+#include "storage_types.hpp"
+
+Package PackageQueue::pop() {
+    //Implementuję zmienną package
+    Package package;
+
+    switch(queue_type) {
+        //Jeśli typ kolejki to LIFO (last in, first out)
+        case PackageQueueType::LIFO:
+           package = std::move(queue.back());
+           //Pobierz ostatni element z kolejki
+           queue.pop_back();
+           //Zwróć zmienną
+           return package;
+
+        //Jeśli typ kolejki to FIFO (first in, first out)
+        case PackageQueueType::FIFO:
+           package = std::move(queue.front());
+           //Pobierz pierwszy element z kolejki
+           queue.pop_front();
+           //Zwróć zmienną
+           return package;
+    }
+}
