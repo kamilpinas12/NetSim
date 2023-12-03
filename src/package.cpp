6263693179f56ddfff4bca936a68c8a2ff2e3c51
Kamil Pinas <kamil.pinas.pl@gmail.com>
2023-12-03 13:55:01 +0100

    x

--- src/package.cpp
+++ src/package.cpp
@@ -4,40 +4,48 @@
 
 #include"package.hpp"
 
-std::set<ElementID> Package::assigned_IDs;
-std::set<ElementID> Package::freed_IDs;
+// Inicializowanie statycznych zmiennych jako puste zbiory
+std::set<ElementID> Package::assigned_IDs = {};
+std::set<ElementID> Package::freed_IDs = {};
 
 
 Package::Package() {
-    //TODO: Jakiś konstruktor  (Kamil)
     //jeżeli coś jest w zwolnionych id to przypisujemy najmniejszą wartość ze zwolniopnych
-    if(freed_IDs.size()){
+    // Zamieniłem metode size() na metodę empty() bo jest adekwatniejsza w tym przypadku (Filip)
+    if(!freed_IDs.empty()){
         id_ = *freed_IDs.begin();
         freed_IDs.erase(id_);
     }
-    //jeżeli zwolnione id są puste nowe id to o 1 większe od największego do tej
-    //przypisanego
-    else if(assigned_IDs.size()){
+        //jeżeli zwolnione id są puste nowe id o 1 większe od największego przypisanego
+    else if(!assigned_IDs.empty()){
         id_ = *assigned_IDs.end() + 1;
     }
-    //pierwszy element dostaje id = 1 (nie wiem czy id zaczynają się od 0 czy od 1
-    //dałem 1 jak coś wystarczy zmienić poniżej)
+        //pierwszy element dostaje id = 1 (nie wiem czy id zaczynają się od 0 czy od 1
+        //dałem 1 jak coś wystarczy zmienić poniżej)
     else{
         id_ = 1;
     }
     assigned_IDs.insert(id_);
 }
 
-
-Package &Package::operator=(Package&& package) {
-    //TODO: Nie mam pojęcia co to kurna jest ale trzeba to napisac
-
+// Przenoszący operator przypisania (zamienia referencje do r-wartości na referencje do l-wartości)(move assignment operator)
+Package &Package::operator=(Package &&package) {
+    //TODO: (Filip)
+    if (this != &package){
+        // Przeniesienie z assigned_IDs do freed_IDs
+        assigned_IDs.erase(this->id_);
+        freed_IDs.insert(this->id_);
+        // Przypisanie nowych wartości
+        this->id_ = package.id_;
+        assigned_IDs.insert(package.id_);
+        return *this;
+    }
+    return *this;
 }
 
 
 Package::~Package() {
-    //TODO: Destruktor przenoszący z assigned_IDs do freed_IDs  (Kamil)
-
+    //git  (Kamil)
     assigned_IDs.erase(id_);
     freed_IDs.insert(id_);
 }
