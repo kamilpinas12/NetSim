6c86824dec1868c3155d72b159377542d2bb8fb0
Kamil Pinas <kamil.pinas.pl@gmail.com>
2023-12-03 13:48:20 +0100

    wirtualne iteratory

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -9,8 +9,8 @@
 #include <list>
 #include "package.hpp"
 
-
-enum PackageQueueType{
+// waydaje mi się że ma być enum class
+enum class PackageQueueType{
     FIFO,
     LIFO
 };
@@ -23,25 +23,25 @@ public:
     virtual  std::size_t size() = 0;
     virtual ~IPackageStockpile() = 0;
     using const_iterator = std::list<Package>::const_iterator;
-    //TODO: Napisać konstruktory tej klasy
+    //TODO: Napisać iteratorytej klasy
 
     //iteratory  (nie jestem pewien czy o to chodzi)
-    const_iterator begin() const {return queue.cbegin();}
-    const_iterator cbegin() {return queue.cbegin();}
-    const_iterator end() const {return queue.end();}
-    const_iterator cend() {return queue.cend();}
-
-private:
-    std::list<Package> queue;
+    // Wydaje mi się że te iteratory muszą być wirtualne bo ta klasa jest w pełni wirtualna
+    // przeniosłem implementacje tych iteratorów do PackageQueue bo faktycznie tutaj muszą być tylko wirtualne
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator end() const = 0;
+    virtual const_iterator cend() const = 0;
 
 };
-
 
 class IPackageQueue: public IPackageStockpile{
 public:
     virtual Package pop() = 0;
     virtual PackageQueueType get_queue_type() = 0;
 
+private:
+    std::list<Package> queue;
 };
 
 
@@ -50,6 +50,14 @@ public:
     //TODO: Zaimplementować metody, zmienne itp. tej klasy
 
 
+    //git Kamil (przeniesione z klasy IPackageStockpile)
+    const_iterator begin() const {return queue.cbegin();}
+    const_iterator cbegin() const {return queue.cbegin();}
+    const_iterator end() const {return queue.end();}
+    const_iterator cend() const {return queue.cend();}
+
+private:
+    std::list<Package> queue;
 };
 
 
