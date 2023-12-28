4efdebb4652eb4fd90b74bd48683921b73bdbb47
MRach3451 <127220659+MRach3451@users.noreply.github.com>
2023-12-28 16:12:38 +0100

    Klasy do simulate
    Zrobiłam klasy do symulacji. Nie buduje się, ale to dlatego, że nie ma jeszcze funkcji simulate()

---
+++ include/simulation.hpp
@@ -0,0 +1,27 @@
+//
+// Created by maria on 28.12.2023.
+//
+
+#ifndef LAB_NETSIM_SIMULATION_HPP
+#define LAB_NETSIM_SIMULATION_HPP
+
+#include "factory.hpp"
+
+class IntervalReportNotifier {
+public:
+    explicit IntervalReportNotifier (TimeOffset to) : to_(to) {};
+    bool should_generate_report(Time t) {return t == 1 || t % to_ == 1; }
+
+private:
+    TimeOffset to_;
+};
+
+class SpecificTurnsReportNotifier {
+public:
+    explicit SpecificTurnsReportNotifier(std::set<Time> turns) : turns_(turns) {};
+    bool should_generate_report(Time t) {return turns_.find(t) != turns_.cend(); };
+private:
+    std::set<Time> turns_;
+};
+
+#endif //LAB_NETSIM_SIMULATION_HPP
